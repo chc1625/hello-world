459efa348cd7a72e264b827fe2ae7cebf758443d
chc1625 <chc1625@163.com>
2017-12-01 11:05:28 +0800

    Create sync_repo.sh

---
+++ sync_repo.sh
@@ -0,0 +1,19 @@
+localpath="/var/www/html/centos"
+mirror="rsync.mirrors.ustc.edu.cn::centos"
+rsync="rsync -avzH --delete"
+verlist="7"
+#baselist="centosplus extras fasttrack os updates"
+baselist="centosplus extras fasttrack os updates storage virt"
+archlist="x86_64"
+for ver in $verlist
+do
+        for base in $baselist
+        do
+                for arch in $archlist
+                do
+                        remote=$mirror/$ver/$base/$arch/
+                        mkdir -pv $localpath/$ver/$base/$arch/
+                        $rsync $remote $localpath/$ver/$base/$arch/
+                done
+        done
+done 
