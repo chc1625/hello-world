491d128296ee44165be483e6dc2b612fda6ca49c
chc1625 <chc1625@163.com>
2017-12-01 14:42:42 +0800

    Update sync_repo.sh

--- sync_repo.sh
+++ sync_repo.sh
@@ -17,3 +17,26 @@ do
                 done
         done
 done 
+
+
+#!/usr/bin/bash
+
+localpath="/var/www/html/ceph-rpm"
+mirror="rsync.mirrors.ustc.edu.cn::ceph"
+rsync="rsync -avzH --delete"
+verlist="rpm-luminous"
+#baselist="centosplus extras fasttrack os updates"
+baselist="el7"
+archlist="x86_64"
+for ver in $verlist
+do
+        for base in $baselist
+        do
+                for arch in $archlist
+                do
+                        remote=$mirror/$ver/$base/$arch/
+                        mkdir -pv $localpath/$ver/$base/$arch/
+                        $rsync $remote $localpath/$ver/$base/$arch/
+                done
+        done
+done 
