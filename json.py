9b309b3fac290ae653fd90013c313bff877eb056
chc1625 <chc1625@163.com>
2018-01-16 11:32:12 +0800

    Update json.py

--- json.py
+++ json.py
@@ -23,3 +23,8 @@ print(json.dumps(s,default=lambda obj:obj.__dict__))
 ##  之 所以无法把Student类实例序列化为JSON，是因为默认情况下，dumps()方法不知道如何将Student实例变为一个JSON的{}对象。
 
 ## 可选参数default就是把任意一个对象变成一个可序列为JSON的对象
+
+
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+json.loads(json_str)
+
