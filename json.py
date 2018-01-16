cd061a786949d5cb6e402c2b6d8b143bbca4c2c6
chc1625 <chc1625@163.com>
2018-01-16 11:27:45 +0800

    create

---
+++ json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+def student2dict(std):
+    return {
+        'name': std.name,
+        'age': std.age,
+        'score': std.score
+    }
+
+s = Student('Bob', 20, 88)
+print(json.dumps(student2dict(s)))
+print(json.dumps(s,default=lambda obj:obj.__dict__))
+
+
+##  之 所以无法把Student类实例序列化为JSON，是因为默认情况下，dumps()方法不知道如何将Student实例变为一个JSON的{}对象。
+
+## 可选参数default就是把任意一个对象变成一个可序列为JSON的对象
